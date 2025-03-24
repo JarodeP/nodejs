6cae4882a777257010594a02414fe7446f79a6ad
Jarode Pottin <Pottinjarode@gmail.com>
2025-03-24 14:29:09 +0100

    commit

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+// Configuration EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'www/views'));  // Changé ici
+app.set('layout', 'layouts/main');
+
 // Configuration des fichiers statiques
-app.use(express.static(__dirname + '/www'));
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use(express.static(path.join(__dirname, 'www')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/www/index.html');
-});
-
-// Démarrage du serveur
-app.listen(port, () => {
-    console.log('Le serveur est en route');
-    console.log(`Serveur accessible à l'adresse : http://localhost:${port}`);
+    res.render('index', {
+        title: 'MonSite',
+        description: 'Un site créé avec Bootstrap et Node.js'
+    });
 });
